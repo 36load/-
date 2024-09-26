94aab27861ddd66030568b78ac7aaddcb644d890
36load <alattas.m1+1@gmail.com>
2024-09-26 09:05:06 -0300

    Add files via upload

--- SaveDiskSerials.bat
+++ SaveDiskSerials.bat
@@ -1,6 +1,32 @@
 @echo off
 StartLocal
 
+:: BatchGotAdmin
+:-------------------------------------
+
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
 :: Define file paths
 Del "C:\Windows\Temp\currentSerials.txt"
 mkdir "C:\Windows\Temp"
